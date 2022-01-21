e9f38bdfd004579e4d3a35ab132ef9595cd3c329
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-21 16:24:42 -0600

    completed part 2

--- src/main/java/Cohort.java
+++ src/main/java/Cohort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Cohort {
@@ -11,17 +12,20 @@ public class Cohort {
 
     public double getCohortAverage() {
         double avg = 0;
-        for (Student student: this.getStudents()) {
+        for (Student student : this.getStudents()) {
             avg += student.getGradeAverage();
         }
         return avg / this.getStudents().size();
     }
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
         students.add(student);
     }
 
     public List<Student> getStudents() {
         return students;
     }
+
+
+
 }
---
+++ src/test/java/CohortTest.java
@@ -0,0 +1,47 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CohortTest {
+    Cohort emptyCohort;
+    Cohort cohortWithOne;
+    Cohort cohortWithMany;
+
+    @Before
+    public void setUp(){
+    emptyCohort = new Cohort();
+
+    cohortWithOne = new Cohort();
+    Student Michael = new Student(1, "Michael");
+    Michael.addGrade(94);
+    Michael.addGrade(90);
+    Michael.addGrade(98);
+    cohortWithOne.addStudent(Michael);
+
+    cohortWithMany = new Cohort();
+    Student Morgan = new Student(2, "Morgan");
+    Morgan.addGrade(96);
+    Morgan.addGrade(90);
+    Morgan.addGrade(93);
+
+    cohortWithMany.addStudent(Michael);
+    cohortWithMany.addStudent(Morgan);
+    }
+    @Test
+    public void addStudents(){
+        Assert.assertEquals("Michael", cohortWithOne.getStudents().get(0).getName());
+        Assert.assertEquals("Morgan", cohortWithMany.getStudents().get(1).getName());
+
+    }
+    @Test
+    public void getStudents(){
+        Assert.assertEquals(1, cohortWithOne.getStudents().size());
+        Assert.assertEquals(2, cohortWithMany.getStudents().size());
+    }
+    @Test
+    public void averageGrade(){
+        Assert.assertEquals(93.5, cohortWithMany.getCohortAverage(),0);
+    }
+
+
+}
