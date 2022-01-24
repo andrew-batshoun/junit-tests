895279332e488b72d22e51a39448c2730305a5e9
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-24 09:39:16 -0600

    refactored code

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,7 +1,5 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,21 +8,11 @@ public class StudentTest {
   @Test
   public void createObj(){
     Student Christian = new Student("Christian", 1);
-    Student Heath = null;
-    Assert.assertNotNull(Christian);
-    Assert.assertNull(Heath);
-  }
-  @Test
-  public void retrieveObj(){
-    Student Christian = new Student("Christian", 1);
-    Christian.addGrade(90);
-    Christian.addGrade(95);
-    Christian.addGrade(91);
-    Christian.addGrade(98);
-    Assert.assertEquals(1, Christian.getId());
     Assert.assertEquals("Christian", Christian.getName());
-    Assert.assertEquals(4, Christian.getGrades().size());
+    Assert.assertEquals(1, Christian.getId());
+    Assert.assertTrue(Christian.getGrades().isEmpty());
   }
+
   @Test
   public void addingGrade(){
     Student Christian = new Student("Christian", 1);
@@ -32,11 +20,12 @@ public class StudentTest {
     Christian.addGrade(95);
     Christian.addGrade(91);
     Christian.addGrade(98);
+    //Assert.assertFalse(Christian.getGrades().isEmpty());
     Assert.assertSame(95, Christian.getGrades().get(1));
     Assert.assertSame(98, Christian.getGrades().get(3));
   }
   @Test
-  public void Average(){
+  public void averageTest(){
     Student Christian = new Student("Christian", 1);
     Christian.addGrade(90);
     Christian.addGrade(95);
@@ -44,5 +33,6 @@ public class StudentTest {
     Christian.addGrade(98);
     Assert.assertEquals(93.5, Christian.getGradeAverage(), 0);
   }
+
 
 }
