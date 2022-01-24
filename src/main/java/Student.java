674e8518fb8311e18eb9a31e27703e91dd55ebe2
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-24 09:06:28 -0600

    refactored bonus methods

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Student {
-    public Scanner scan = new Scanner(System.in);
+
     private String name;
     private long id;
     private ArrayList<Integer> grades;
@@ -27,23 +27,20 @@ public class Student {
         return this.grades;
     }
 
-    public Integer updateGrade(){
-        System.out.println("Which grade would you like to replace?");
-        int oldGrade = scan.nextInt();
-        System.out.println("What is the new grade?");
-        int newGrade = scan.nextInt();
-        return grades.set(oldGrade,newGrade);
+    public Integer updateGrade(int index, int newGrade){
+
+        return grades.set(index,newGrade);
     }
 
-    public Integer deleteGrade(){
-        System.out.println("Which grade would you like to delete?");
-        int delGrade = scan.nextInt();
-        return grades.remove(delGrade);
+    public Integer deleteGrade(int index){
+
+        return grades.remove(index);
     }
 
     public void addGrade(int grade) {
         this.grades.add(grade);
     }
+
     public double getGradeAverage(){
         double sum = 0;
         for(int grade: this.grades){
